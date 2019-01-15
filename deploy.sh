85fcbb122b79dadca340a78891e8853e35aac68d
JingJingJingJing <summerrymaker@gmail.com>
2019-01-15 18:59:52 +0800

    send docker yml file to server

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-ssh root@$server_address <<EOF
+IP = 128.199.81.210
+DEPLOY_DIR = /ks
+
+scp app/docker-compose-total.yml username@a:$DEPLOY_DIR/docker-compose-total.yml 
+
+ssh root@$IP <<EOF
+    cd $DEPLOY_DIR
     if [ "$(docker ps -a -q)" ]; then
         docker stop $(docker ps -a -q)
         docker rm $(docker ps -a -q)
     fi
-    docker-compose -f ./app/docker-compose-total.yml -p mongodb up -d && bash ./app/setup.sh
+    docker-compose -f app/docker-compose-total.yml -p mongodb up -d && bash ./app/setup.sh
 EOF
