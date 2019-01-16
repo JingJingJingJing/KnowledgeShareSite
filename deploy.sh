155a8389ca4fa4cbf245f2401d43f087188c04d0
JingJingJingJing <summerrymaker@gmail.com>
2019-01-16 16:32:47 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,11 +14,6 @@ scp -oStrictHostKeyChecking=no -rp app root@128.199.81.210:/ks
 
 ssh -oStrictHostKeyChecking=no root@128.199.81.210 <<EOF
     cd /ks/app
-    if [ "$(docker ps -a -q)" ]; then
-        echo "exist running containers"
-        docker stop $(docker ps -a -q)
-        docker rm $(docker ps -a -q)
-    fi
     docker-compose -f docker-compose-total.yml -p mongodb up -d && bash setup.sh
 EOF
     
