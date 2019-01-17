21d99b926e5cdcf474efc399ef76bd8b0cfb32d8
JingJingJingJing <summerrymaker@gmail.com>
2019-01-17 09:56:58 +0800

    docker pull before docker-compose up

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,7 @@ scp -oStrictHostKeyChecking=no -rp app root@$server_address:$DEPLOY_DIR
 
 ssh -oStrictHostKeyChecking=no root@$server_address <<EOF
     cd $DEPLOY_DIR/app
+    docker pull gemmawu/sasharingapp:1.0
     docker-compose -f docker-compose-total.yml -p mongodb up -d && bash setup.sh
 EOF
     
