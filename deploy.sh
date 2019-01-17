d8faba410e5da9762d250c01fd356e9ce9924759
JingJingJingJing <summerrymaker@gmail.com>
2019-01-17 10:19:22 +0800

    docker pull before docker-compose up

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,7 @@ scp -oStrictHostKeyChecking=no -rp app root@$server_address:$DEPLOY_DIR
 
 ssh -oStrictHostKeyChecking=no root@$server_address <<EOF
     cd $DEPLOY_DIR/app
+    echo $docker_hub_gemma_pw | docker login -u $docker_hub_gemma --password-stdin
     docker pull gemmawu/sasharingapp:1.0
     docker-compose -f docker-compose-total.yml -p mongodb up -d && bash setup.sh
 EOF
